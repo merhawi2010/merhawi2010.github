9cbc098bfae2daeadc3d956f2cf691dcd4e56807
merhawi <meraxt@gmail.com>
2017-09-30 22:01:43 +0200

    optimized content from pagespeed added

--- views/js/main.js
+++ views/js/main.js
@@ -498,15 +498,20 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
+
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
   var multiplier = (document.body.scrollTop / 1250);
+  var phase = [];
+  for (var j = 0; j < items.length; j++) {
+    phase.push(Math.sin(multiplier + (j % 5)));
+  }
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(multiplier + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+/*    var phase = Math.sin(multiplier + (i % 5));*/
+    items[i].style.left = items[i].basicLeft + 100 * phase[i] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -527,7 +532,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 20; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -539,3 +544,6 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   updatePositions();
 });
+
+
+
